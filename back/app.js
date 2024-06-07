0dbe86390bbad3bce857c7cdfc0188d7e02e4db8
AlexDziunyk <alexdziunyk@gmail.com>
2024-06-07 03:03:41 +0300

    Added function of creating ai image

--- back/app.js
+++ back/app.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+require('dotenv').config();
 
+const openaiRoutes = require('./routes/openaiRoutes');
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/openai', openaiRoutes);
 
 app.listen(3001, () => {
   console.log("Listening...");
---
+++ back/routes/openaiRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+const { generateImage } = require('../controllers/openaiController');
+
+router.post('/generate-image', generateImage);
+
+
+module.exports = router;

93cca913dab6d745b79b0c7c6afc90e3394780d9
Fallen-Eleonor <Daniil.Dunai@cs.khpi.edu.ua>
2024-06-11 00:22:59 +0300

    add login, home and registration pages + routes

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,17 +1,27 @@
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import MyBoard from './components/MyBoard/MyBoard';
 import Toolbar from './components/Toolbar/Toolbar';
 import Scene from './pages/Scene/Scene';
-
+import HomePage from './pages/HomeScene/HomeScene';
+import LoginPage from './pages/LoginScene/LoginScene';
+import SignUpPage from './pages/SignUpScene/SignUpScene';
 
 function App() {
-
-
   return (
-    <div className='app'>
-      <Scene />
-    </div>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/scene" element={<Scene />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App
+
+

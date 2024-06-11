6f617bd4d47ed610d7d707febd22b154a01d0401
AlexDziunyk <alexdziunyk@gmail.com>
2024-06-11 21:34:13 +0300

    New design for board

--- front/src/components/ToolsItem/ToolsItem.jsx
+++ front/src/components/ToolsItem/ToolsItem.jsx
@@ -8,13 +8,15 @@ import './style.scss';
 const ToolsItem = ({ selectedShape, onChange, moveHigher, moveLower }) => {
 
   return (
-    <div className='tools-item'>
-      {selectedShape && <div className='items'>
-        {selectedShape.type !== "text" && <ShapesToolbar selectedShape={selectedShape} onChange={onChange} />}
-        {selectedShape.type === "text" && <TextToolbar selectedShape={selectedShape} onChange={onChange} />}
-        <div onClick={() => moveHigher(selectedShape)} className='icon-box'><FaLongArrowAltUp /></div>
-        <div onClick={() => moveLower(selectedShape)} className='icon-box'><FaLongArrowAltDown /></div>
-      </div>}
+    <div className='tools__container'>
+      <div className='tools-item'>
+        {selectedShape && <div className='items'>
+          {selectedShape.type !== "text" && <ShapesToolbar selectedShape={selectedShape} onChange={onChange} />}
+          {selectedShape.type === "text" && <TextToolbar selectedShape={selectedShape} onChange={onChange} />}
+          <div onClick={() => moveHigher(selectedShape)} className='icon-box'><FaLongArrowAltUp /></div>
+          <div onClick={() => moveLower(selectedShape)} className='icon-box'><FaLongArrowAltDown /></div>
+        </div>}
+      </div>
     </div>
   )
 }

bc5e6429dce5c5138e06830770c9b03193180eff
AlexDziunyk <alexdziunyk@gmail.com>
2024-06-12 15:43:19 +0300

    Fixes to back and navbar

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
 
 import MyBoard from './components/MyBoard/MyBoard';
@@ -9,17 +9,43 @@ import HomePage from './pages/HomeScene/HomeScene';
 import LoginPage from './pages/LoginScene/LoginScene';
 import SignUpPage from './pages/SignUpScene/SignUpScene';
 import RootPage from './pages/RootPage/RootPage';
+import axios from './axios/axios';
 
 function App() {
 
   const router = createBrowserRouter(createRoutesFromElements(
     <Route path="/" element={<RootPage />}>
-      <Route element={<HomePage />} />
+      <Route index element={<HomePage />} />
       <Route path="login" element={<LoginPage />} />
       <Route path="signup" element={<SignUpPage />} />
-      <Route index element={<Scene />} />
+      <Route element={<Scene />} />
     </Route>
-  ))
+  ));
+
+  const testApi = async () => {
+    const result = await axios.post("/user/register", {
+      login: "alex",
+      password: "pass",
+      email: "alexdziunyk@gmail.com"
+    });
+
+    console.log(result);
+
+  }
+
+  const login = async () => {
+    const result = await axios.post("/user/login", {
+      login: "alex",
+      password: "pass",
+    });
+
+    console.log(result);
+
+  }
+
+  useEffect(() => {
+    login()
+  }, []);
 
   return (
     <div className='app'>
--- front/src/components/Navbar/Navbar.jsx
+++ front/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import './style.scss';
 import logo from '../../assets/Logo.svg';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
@@ -8,10 +9,10 @@ const Navbar = () => {
         <img src={logo} alt='logo'></img>
       </div>
       <div className='navbar__links'>
-        <div className='navbar__links_item'>Docs</div>
-        <div className='navbar__links_item'>Support</div>
-        <div className='navbar__links_item'>Log In</div>
-        <div className='navbar__links_button'>Sign Up</div>
+        <Link><div className='navbar__links_item'>Docs</div></Link>
+        <Link><div className='navbar__links_item'>Support</div></Link>
+        <Link className='link' to="/login"><div className='navbar__links_item'>Log In</div></Link>
+        <Link className='link' to="/signup"><div className='navbar__links_button'>Sign Up</div></Link>
       </div>
     </div>
   )

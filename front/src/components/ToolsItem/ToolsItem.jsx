163924a1413c3cec836458fd7a213db8b9da183f
AlexDziunyk <alexdziunyk@gmail.com>
2024-06-14 02:04:29 +0300

    Fixes to images toolbar

--- front/src/components/ToolsItem/ToolsItem.jsx
+++ front/src/components/ToolsItem/ToolsItem.jsx
@@ -3,6 +3,7 @@ import TextToolbar from './components/TextToolbar';
 import { FaLongArrowAltUp } from "react-icons/fa";
 import { FaLongArrowAltDown } from "react-icons/fa";
 import './style.scss';
+import ImagesToolbar from './components/ImagesToolbar';
 
 
 const ToolsItem = ({ selectedShape, onChange, moveHigher, moveLower }) => {
@@ -12,7 +13,8 @@ const ToolsItem = ({ selectedShape, onChange, moveHigher, moveLower }) => {
       {selectedShape && <div className='tools__container'>
         <div className='tools-item'>
           <div className='items'>
-            {selectedShape.type !== "text" && <ShapesToolbar selectedShape={selectedShape} onChange={onChange} />}
+            {selectedShape.type === "image" && <ImagesToolbar selectedShape={selectedShape} onChange={onChange} />}
+            {(selectedShape.type === "rect" || selectedShape.type === "star" || selectedShape.type === "circle") && <ShapesToolbar selectedShape={selectedShape} onChange={onChange} />}
             {selectedShape.type === "text" && <TextToolbar selectedShape={selectedShape} onChange={onChange} />}
             <div onClick={() => moveHigher(selectedShape)} className='icon-box'><FaLongArrowAltUp /></div>
             <div onClick={() => moveLower(selectedShape)} className='icon-box'><FaLongArrowAltDown /></div>
